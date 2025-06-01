830517319071ed22dc580f161a40cb1177d51519
Jiahao, Woo <woojiahao1234@gmail.com>
2025-06-01 20:45:02 +0800

    Fix echo

--- hands-on/initializing-local-repo.sh
+++ hands-on/initializing-local-repo.sh
@@ -3,5 +3,5 @@
 if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   echo -e 'The current project \033[1mis\033[0m a Git repository'
 else
-  echo 'The current project \033[1mis not\033[0m a Git repository'
+  echo -e 'The current project \033[1mis not\033[0m a Git repository'
 fi
